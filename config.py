08329f5943302188b4137222308f329bbbf388cc
Devapriya <devapriya1220@gmail.com>
2026-08-04 00:13:30 +0530

    New commit

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = "postgresql://postgres:root@localhost:5432/job_tracker"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "jobtracker2026secretkey"
+    SECRET_KEY = "---"
